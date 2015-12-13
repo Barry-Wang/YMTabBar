17a8b4a1e996c6177a63d4fcc40bf4c7708d0e1f
Barry <barrywang23@gmail.com>
2015-12-13 20:13:30 +0800

    Initial Commit

---
+++ YMTabBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YMTabBar
+//
+//  Created by YmWw on 15/12/13.
+//  Copyright © 2015年 Barry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
